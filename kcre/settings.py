fdda18d2d4bf39934311c6a20eb7207ff4cca891
Natalie <natalieschennum@gmail.com>
2019-03-29 13:54:14 -0700

    add listings to installed apps

--- kcre/settings.py
+++ kcre/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
