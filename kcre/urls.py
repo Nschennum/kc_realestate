9822742cebba23ec64cc709124273caa0425673f
Natalie <natalieschennum@gmail.com>
2019-03-29 15:48:28 -0700

    update main urls

--- kcre/urls.py
+++ kcre/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')), 
     path('listings/', include('listings.urls')), 
+    path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
