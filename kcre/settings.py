3e6e384856807a939dcead53580e5339ddfd63b0
Natalie <natalieschennum@gmail.com>
2019-03-29 15:47:59 -0700

    +humanize, media, messages

--- kcre/settings.py
+++ kcre/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,20 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'kcre/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+# Email config for admin to recieve and email upon Inquiry
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER=''
+# EMAIL_HOST_PASSOWRD=''
+# EMAIL_USE_TLS=True
